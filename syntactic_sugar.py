2dc9f6fe7a9b5db2c55ef2c49c5f3db11e1e498a
Eugene Moon <uoojin95@gmail.com>
2019-01-08 17:27:16 -0500

    added some syntactic sugar examples

---
+++ syntactic_sugar.py
@@ -0,0 +1,16 @@
+'''
+just some examples of cool syntacs
+'''
+
+list1 = [1,2,3,4]
+list2 = range(1,5)
+print list1, list2
+
+mapList1 = [item*2 for item in list1]
+print mapList1
+
+filterList1 = [item for item in list1 if item%2 == 0]
+print filterList1
+
+filterAndMapList1 = [item*3 for item in list1 if item%2 == 1]
+print filterAndMapList1
