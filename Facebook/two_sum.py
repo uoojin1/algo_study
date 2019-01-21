b5fa4bfd626c6d5e78ca824064e1f678c9c593c7
Eugene Moon <uoojin95@gmail.com>
2019-01-20 21:55:04 -0500

    solved two sum

---
+++ Facebook/two_sum.py
@@ -0,0 +1,18 @@
+''' Two sum
+
+given an array of integers, return indices of the two numbers such that they add up to a specfic target
+
+you may assume that each input would have exactly one solution, and you may not use the same
+element twice
+'''
+
+def twoSum(nums, target):
+    history = {}
+    for i, num in enumerate(nums):
+        if target - num in history:
+            return (history[target-num], i)
+        else:
+            history[num] = i
+    return None
+
+print twoSum([2,7,11,15], 22)
